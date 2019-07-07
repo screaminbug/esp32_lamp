6e3d7894f8007ec77c7a5d5af7d97bbac17a4d80
Tomislav <tstrelar@gmail.com>
2019-07-07 19:48:10 +0200

    Add header example file

---
+++ secret_example.h
@@ -0,0 +1,7 @@
+#define SSID "Your WiFi SSID"
+#define PWD "Your WiFi password"
+#define BRIDGE "Bridge ip/hostname"
+#define API_USERNAME "Your Hue username"
+#define LIGHTS_COUNT 6
+#define LIGHT_IDS "2,3,5,6,7,8"     //comma delimited light ids, must match LIGHT_COUNT number
+#define BUTTON_PINS "1,2,3,4,5,6"   //comma delimited input pins, must match LIGHT_COUNT number, pin index determines which light will be controlled
